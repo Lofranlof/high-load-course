4733d81f94dcdc8e0b4281100ff066610785a5a4
Ruslan Davletov <r.davletov@crpt.ru>
2025-02-18 23:35:13 +0300

    solved 1st case

--- src/main/kotlin/ru/quipy/payments/config/PaymentAccountsConfig.kt
+++ src/main/kotlin/ru/quipy/payments/config/PaymentAccountsConfig.kt
@@ -32,6 +32,7 @@ class PaymentAccountsConfig {
     private val allowedAccounts = setOf("acc-9")
 
 
+
     @Bean
     fun accountAdapters(paymentService: EventSourcingService<UUID, PaymentAggregate, PaymentAggregateState>): List<PaymentExternalSystemAdapter> {
         val request = HttpRequest.newBuilder()
